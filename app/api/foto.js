425549302764ebe38b7482c24707e6382f82ac38
Nickrosendo <nicolasrosendo@hotmail.com>
2017-11-22 01:03:27 -0200

    Mongoose

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,42 +1,43 @@
-const api = {}
+const mongoose = require("mongoose");
 
-const fotos = [
-    { _id: 1, titulo: 'Leão', url: 'http://www.fundosanimais.com/Minis/leoes.jpg', descricao: 'Esse é um Leão africano muito selvagem' },
-    { _id: 2, titulo: 'Leão 2', url: 'http://www.fundosanimais.com/Minis/leoes.jpg',  descricao: 'Esse é um Leão asiatico muito selvagem' }
-  ]
+const api = {};
 
-let contador = fotos.length
+// let contador = fotos.length
 
-api.lista = (req, res) => {  
-  res.json(fotos)
-}
+api.lista = (req, res) => {
+  const model = mongoose.model("Foto");
+  model.find({}).then(
+    fotos => res.json(fotos),
+    error => {
+      console.log(error)
+      res.sendStatus(500).json(error);
+    }
+  );
+};
 
 api.adiciona = (req, res) => {
-  let foto = req.body;
-  foto._id = ++contador;
-  fotos.push(foto);
-  res.json(foto);
-}
+  // let foto = req.body;
+  // foto._id = ++contador;
+  // fotos.push(foto);
+  // res.json(foto);
+};
 
 api.atualiza = (req, res) => {
-  let foto = req.body;
-  fotoId = req.params.id;
-  let indice = fotos.findIndex( foto => foto._id == fotoId);
-
-  fotos[indice] = foto;
-
-  res.sendStatus(200);
-}
+  // let foto = req.body;
+  // fotoId = req.params.id;
+  // let indice = fotos.findIndex( foto => foto._id == fotoId);
+  // fotos[indice] = foto;
+  // res.sendStatus(200);
+};
 
 api.buscaPorId = (req, res) => {
-  let foto = fotos.find( foto => foto._id == req.params.id)
-
-  res.json(foto)
-}
+  // let foto = fotos.find( foto => foto._id == req.params.id)
+  // res.json(foto)
+};
 
 api.removePorId = (req, res) => {
-  let foto = fotos.filter( foto => foto._id != req.params.id)
-  res.sendStatus(204)
-}
+  // let foto = fotos.filter( foto => foto._id != req.params.id)
+  // res.sendStatus(204)
+};
 
 module.exports = api;
